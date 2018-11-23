b118cc28116c03375580f28944f22dfad6a14178
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2018-11-23 15:26:50 +0100

    Add API service using axios to handle HTTP requests for  the integration of RESTful microservices. Add GET HTTP method to API service.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const helmet     = require('helmet');
 const api     = require('./src/api/index');
 const config     = require('./src/config');
+const ApiService = require('./src/services/apiService');
 
 const app = express();
 
@@ -22,3 +23,14 @@ app.use(function(req, res, next) {
 app.listen(config.port, function() {
   console.log('Listening on port ' + config.port);
 });
+
+// Example of new ApiService
+let endorseNlpService = new ApiService(config.nlpServiceBaseUrl, 'application/json', 'application/json', 'Basic', '');
+let func = async () => {
+  try {
+    const response = await endorseNlpService.get('/api/1/spacy/hello', '');
+    console.log(response)
+  } catch (e) {
+    console.log(e);
+  }
+};
